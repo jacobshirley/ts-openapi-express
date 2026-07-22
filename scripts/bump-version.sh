84dc92b1ab089d9b6fe09e9cf2748c856d5ce03f
Jacob ‘Jake’ Shirley <jakeshirley2@gmail.com>
2026-07-22 18:18:34 +0100

    fix: use npm version instead of pnpm version for --no-git-tag-version compatibility

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -8,10 +8,10 @@ set -e
 
 if [ -n "$1" ]; then
   # Bump version in root package.json
-  pnpm version --no-git-tag-version $@
+  npm version --no-git-tag-version $@
 
   # Bump version in all workspaces
-  pnpm -r exec pnpm version --no-git-tag-version $@
+  pnpm -r exec npm version --no-git-tag-version $@
 
   # Get the new version from root package.json
   NEW_VERSION=$(node -p "require('./package.json').version")
